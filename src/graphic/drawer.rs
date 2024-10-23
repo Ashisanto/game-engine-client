aabf3eb4b2b631d7e304a9a80be724bbeff94472
aunggoonarpasunun <aunggoon@erx.io>
2024-10-23 20:15:17 +0700

    bevy install

--- src/graphic/drawer.rs
+++ src/graphic/drawer.rs
@@ -11,7 +11,7 @@ pub fn render_2d() {
         let c_width = 900.0;
 
         thread::spawn(|| {
-            play::play("./assets/music/Final Boss.ogg").unwrap();
+            play::play("./assets/music/FinalBoss.ogg").unwrap();
         });
 
         // Load a file
